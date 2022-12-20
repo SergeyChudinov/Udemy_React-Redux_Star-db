85479bac56934bae6a092ea0076b313bd034933f
Sergey <SergeyChudinov1986@gmail.com>
2022-12-20 19:07:50 +0400

    Make lesson-6-11

---
+++ star-db/src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-x8hjwy56drl">
+        <div className="ldio-kuk99sbabx">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div>
+    </div>
+  );
+};
+
+export default Spinner;
